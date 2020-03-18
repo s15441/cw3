a9442b943588396a2ff50c54e580101122c19777
BuildTools <unconfigured@null.spigotmc.org>
2020-03-18 08:34:43 +0100

    Initial

---
+++ cw3/cw3/Controllers/StudensController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudensController : ControllerBase
+    {
+
+        [HttpGet]
+        public string getStudents()
+        {
+            return "Gasowski, Kowalska";
+        }
+        
+
+        // [HttpGet]
+        // // GET: api/values
+        // public IEnumerable<string> Get()
+        // {
+        //     return new string[] { "value1", "value2" };
+        // }
+
+        //// GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("Siema");
+        }
+
+        //// POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
